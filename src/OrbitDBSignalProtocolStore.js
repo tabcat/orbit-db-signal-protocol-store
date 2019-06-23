3496bb240f06a10e637e0a76faa75fdc21f7a004
tabcat <baseless@gmx.com>
2019-06-23 08:45:54 -0500

    removed es6 features

--- src/OrbitDBSignalProtocolStore.js
+++ src/OrbitDBSignalProtocolStore.js
@@ -1,113 +1,150 @@
 
-const util = require('./helpers.js')
+'use strict'
+var util = require('./helpers.js');
 
-class OrbitDBSignalProtocolStore {
-  constructor(orbitDocstore) {
-    this.store = orbitDocstore
-  }
-
-  Direction = {
-    SENDING: 1,
-    RECEIVING: 2,
-  }
-
-  getIdentityKeyPair = () => Promise.resolve(this.get('identityKey'))
-  getLocalRegistrationId = () => Promise.resolve(this.get('registraionId'))
-
-  put = async (key, value) => {
-    if (key === undefined || value === undefined || key === null || value === null) {
-      throw new Error("Tried to store undefined/null")
-    }
-    return await this.store.put({_id:key, value})
-  }
-  get = async (key, defaultValue) => {
-    if (key === null || key === undefined) {
-      throw new Error("Tried to get value for undefined/null key")
-    }
-    const doc = this.store.query((doc) => doc._id === key)
-    return doc.length !== 0
-    ? doc[0].value
-    : defaultValue
-  }
-  remove = async (key) => {
-    if (key === null || key === undefined) {
-      throw new Error("Tried to remove value for undefined/null key")
-    }
-    return await this.store.del(key)
-  }
-
-  isTrustedIdentity = (identifier, identityKey, direction) {
-    if (identifier === null || identifier === undefined) {
-      throw new Error("tried to check identity key for undefined/null key")
-    }
-    if (!(identityKey instanceof ArrayBuffer)) {
-      throw new Error("Expected identityKey to be an ArrayBuffer")
-    }
-    const trusted = this.get('identityKey' + identifier)
-    return trusted === undefined
-    ? Promise.resolve(true)
-    : Promise.resolve(util.toString(identityKey) === util.toString(trusted))
-  }
-  loadIdentityKey = (identifier) => {
-    if (identifier === null || identifier === undefined) {
-      throw new Error("Tried to get identity key for undefined/null key")
-    }
-    return Promise.resolve(this.get('identityKey' + identifier))
-  }
-  saveIdentity = (identifier, identityKey) => {
-    if (identifier === null || identifier === undefined) {
-      throw new Error("Tried to put identity key for undefined/null key")
-    }
-
-    const address = new libsignal.SignalProtocolAddress.fromString(identifier)
-
-    const existing = this.get('identityKey' + address.getName())
-    this.put('identityKey' + address.getName(), identityKey)
-
-    return existing && util.toString(identityKey) !== util.toString(existing)
-    ? Promise.resolve(true)
-    : Promise.resolve(false)
-  }
-
-  /* Returns a prekeypair object or undefined */
-  loadPreKey = async (keyId) => {
-    const preKey = await this.get('25519KeypreKey' + keyId)
-    const res = preKey !== undefined
-    ? { pubKey: preKey.pubKey, privKey: preKey.privKey }
-    : undefined
-    return Promise.resolve(res)
-  }
-  storePreKey = (keyId, keyPair) => {
-    return Promise.resolve(this.put('25519KeypreKey' + keyId, keyPair))
-  }
-  removePreKey = (keyId) =>
-    Promise.resolve(this.remove('25519KeypreKey' + keyId))
-
-  /* Returns a signed keypair object or undefined */
-  loadSignedPreKey = async (keyId) => {
-    const signedPreKey = await this.get('25519KeysignedKey' + keyId)
-    const res = signedPreKey !== undefined
-    ? { pubKey: signedPreKey.pubKey, privKey: signedPreKey.privKey }
-    : undefined
-    return Promise.resolve(res)
-  }
-  storeSignedPreKey = (keyId, KeyPair) =>
-    Promise.resolve(this.put('25519KeysignedKey' + keyId, keyPair))
-  removeSignedPreKey = (keyId) =>
-    Promise.resolve(this.remove('25519KeysignedKey' + keyId))
-
-  loadSession = (identifier) =>
-    Promise.resolve(this.get('session' + identifier))
-  storeSession = (identifier, record) =>
-    Promise.resolve(this.put('session' + identifier, record))
-  removeSession = (identifier) =>
-    Promise.resolve(this.remove('session' + identifier))
-  removeAllSessions = (indentifier) => {
-    const matches =
-      this.store.query((doc) => doc._id.startsWith('session' + identifier))
-    return Promise.all(matches.map((match) => this.store.del(match._id)))
-  }
-
+function OrbitDBSignalProtocolStore(orbitDocstore) {
+  this.store = orbitDocstore;
 }
 
-module.exports = OrbitDBSignalProtocolStore
+OrbitDBSignalProtocolStore.prototype = {
+  Direction: {
+    SENDING: 1,
+    RECEIVING: 2,
+  },
+
+  getIdentityKeyPair: function() {
+    return Promise.resolve(this.get('identityKey'));
+  },
+  getLocalRegistrationId: function() {
+    return Promise.resolve(this.get('registrationId'));
+  },
+  put: function(key, value) {
+    if (key === undefined || value === undefined || key === null || value === null)
+      throw new Error("Tried to store undefined/null");
+    return Promise.resolve(this.store.put({ _id:key, value:value }));
+  },
+  get: function(key, defaultValue) {
+    if (key === null || key === undefined)
+      throw new Error("Tried to get value for undefined/null key");
+		var doc = this.store.query(function(_doc) { return _doc._id === key })[0];
+    if (doc !== undefined) {
+      return Promise.resolve(doc.value);
+    } else {
+      return Promise.resolve(defaultValue);
+    }
+  },
+  remove: function(key) {
+    if (key === null || key === undefined)
+      throw new Error("Tried to remove value for undefined/null key");
+		return Promise.resolve(this.get(key).then(function(val) {
+			if (val) {
+				return Promise.resolve(this.store.del(key));
+			} else {
+				return Promise.resolve(true);
+			}
+		}.bind(this)));
+  },
+	groupedRemove: function(docs) {
+    var reducedPromises = docs.reduce(function(accu, doc) {
+      return accu.then(function(promises) {
+        var promise = Promise.resolve(this.remove(doc._id))
+        return promise.then(function() {
+          return Promise.resolve(promises.concat([promise]))
+        })
+      }.bind(this)).catch(function(err) {
+        console.error(err)
+        return Promise.resolve(promises)
+      })
+    }.bind(this), Promise.resolve([]))
+    return Promise.resolve(
+      reducedPromises.then(function(promises) {
+        var completed = docs.length === promises.length
+        return Promise.resolve(completed)
+      })
+    )
+	},
+
+  isTrustedIdentity: function(identifier, identityKey, direction) {
+    if (identifier === null || identifier === undefined) {
+      throw new Error("tried to check identity key for undefined/null key");
+    }
+    if (!(identityKey instanceof ArrayBuffer)) {
+      throw new Error("Expected identityKey to be an ArrayBuffer");
+    }
+    var trusted = this.get('identityKey' + identifier);
+    if (trusted === undefined) {
+      return Promise.resolve(true);
+    }
+    return Promise.resolve(util.toString(identityKey) === util.toString(trusted));
+  },
+  loadIdentityKey: function(identifier) {
+    if (identifier === null || identifier === undefined)
+      throw new Error("Tried to get identity key for undefined/null key");
+    return Promise.resolve(this.get('identityKey' + identifier));
+  },
+  saveIdentity: function(identifier, identityKey) {
+    if (identifier === null || identifier === undefined)
+      throw new Error("Tried to put identity key for undefined/null key");
+
+    var address = new libsignal.SignalProtocolAddress.fromString(identifier);
+
+    var existing = this.get('identityKey' + address.getName());
+    this.put('identityKey' + address.getName(), identityKey)
+
+    if (existing && util.toString(identityKey) !== util.toString(existing)) {
+      return Promise.resolve(true);
+    } else {
+      return Promise.resolve(false);
+    }
+
+  },
+
+  /* Returns a prekeypair object or undefined */
+  loadPreKey: function(keyId) {
+    var res = this.get('25519KeypreKey' + keyId);
+    if (res !== undefined) {
+      res = { pubKey: res.pubKey, privKey: res.privKey };
+    }
+    return Promise.resolve(res);
+  },
+  storePreKey: function(keyId, keyPair) {
+    return Promise.resolve(this.put('25519KeypreKey' + keyId, keyPair));
+  },
+  removePreKey: function(keyId) {
+    return Promise.resolve(this.remove('25519KeypreKey' + keyId));
+  },
+
+  /* Returns a signed keypair object or undefined */
+  loadSignedPreKey: function(keyId) {
+    var res = this.get('25519KeysignedKey' + keyId);
+    if (res !== undefined) {
+      res = { pubKey: res.pubKey, privKey: res.privKey };
+    }
+    return Promise.resolve(res);
+  },
+  storeSignedPreKey: function(keyId, keyPair) {
+    return Promise.resolve(this.put('25519KeysignedKey' + keyId, keyPair));
+  },
+  removeSignedPreKey: function(keyId) {
+    return Promise.resolve(this.remove('25519KeysignedKey' + keyId));
+  },
+
+  loadSession: function(identifier) {
+    return Promise.resolve(this.get('session' + identifier));
+  },
+  storeSession: function(identifier, record) {
+    return Promise.resolve(this.put('session' + identifier, record));
+  },
+  removeSession: function(identifier) {
+    return Promise.resolve(this.remove('session' + identifier));
+  },
+  removeAllSessions: function(identifier) {
+		var sessions = this.store.query(function(doc) {
+      return doc._id.startsWith('session' + identifier)
+    });
+    return Promise.resolve(this.groupedRemove(sessions));
+  }
+};
+
+module.exports = OrbitDBSignalProtocolStore;
+
